a5aeaff2f8495a9866b7cc13875b8813267afcd0
dalianmao <478146528@qq.com>
2013-05-22 14:43:16 +0800

    stable version 2013.5.22(plist used)
    use plist to set the mainView's elements.Testing ok.

---
+++ CDDGame/PositionGetter.h
@@ -0,0 +1,13 @@
+//
+//  PositionGetter.h
+//  CDDGame
+//
+//  Created by xc on 13-5-20.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PositionGetter : NSObject
+
+@end
---
+++ CDDGame/PositionGetter.m
@@ -0,0 +1,13 @@
+//
+//  PositionGetter.m
+//  CDDGame
+//
+//  Created by xc on 13-5-20.
+//
+//
+
+#import "PositionGetter.h"
+
+@implementation PositionGetter
+
+@end
